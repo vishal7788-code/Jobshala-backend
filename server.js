7395cb884216fd9b101c08c33c819e44916a3f89
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 08:54:23 +0530

    Update server.js
    typo-error

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://jobshala-project.vercel.app
-",
+  origin: "http://jobshala-project.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
