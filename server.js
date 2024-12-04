cc7d627f840b4cd7d2eb24fcdca24632b56b0426
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 08:51:41 +0530

    Update server.js
    cors update

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const corsOptions = {
 ",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions));
 
