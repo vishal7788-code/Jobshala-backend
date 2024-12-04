8c0c53c807ffc23292c145ecf0188685ba56b9e7
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 09:55:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const corsOptions = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
+  headers:{
+    'Access-Control-Allow-Origin':"http://jobshala-project.vercel.app"
+    },
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); 
