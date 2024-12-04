1c45d6167d223859e949ee15d5d941c2d40d42d2
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 14:28:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://jobshala-project.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
   headers:{
-    'Access-Control-Allow-Origin':"http://jobshala-project.vercel.app"
+    'Access-Control-Allow-Origin':"http://localhost:5173"
     },
 };
 app.use(cors(corsOptions));
