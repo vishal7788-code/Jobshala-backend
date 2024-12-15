1d46d8df192823da4ccc2a20746f49a00d17f3c2
Vishal Mishra <vm174294@gmail.com>
2024-12-15 21:54:16 +0530

    token issue fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // https://jobshala-project.vercel.app
 const corsOptions = {
-  origin: "https://jobshala-project.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
