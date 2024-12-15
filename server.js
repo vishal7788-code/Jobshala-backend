553779536ef5bacdbda46181827c0dff04bd1cb7
Vishal Mishra <vm174294@gmail.com>
2024-12-15 22:24:51 +0530

    cors change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // https://jobshala-project.vercel.app
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: " https://jobshala-project.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
